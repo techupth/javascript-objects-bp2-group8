743b88ee7038b25dbb18857da075277471260e56
Krittin <rubruengyod@gmail.com>
2023-12-28 00:13:55 +0700

    test: Objects assignment

--- ex-2.js
+++ ex-2.js
@@ -1,17 +1,16 @@
-// Exercise #2 Product
-
+// Exercise #2: Product
 const product = {
-  name: "Xiaomi Air Purifier",
-  price: 4000,
-  ratings: 2.5,
-  photo: null,
-};
-let socialMediaShare = "facebookShare";
-// Start code here
+    name: "Xiaomi Air Purifier",
+    price: 4000,
+    ratings: 2.5,
+    photo: null,
+  };
+  let socialMediaShare = "facebookShare";
+  // Start code here
 product.ratings = 4.5;
-product.photo ="https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg"
-product.code = "IT0101010";
+product.photo = "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg"
+product.code = IT0101010;
 product.price = 6000;
+delete product.ratings;
 product[socialMediaShare] = 45.5;
-
 console.log(product);
